cc5a37ae325678c03b2b1174ab1ae4a7ea1ae42f
tihon228supermegaproIT <119882092+tihon228supermegaproIT@users.noreply.github.com>
2023-08-29 14:59:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-const GuessingGame = require('./src/guessing-game.js');
+class GuessingGame {
+  constructor() {
+    this.minRange = null;
+    this.maxRange = null;
+    this.targetNumber = null;
+  }
 
-window.game = new GuessingGame();
+  setRange(min, max) {
+    this.minRange = min;
+    this.maxRange = max;
+    this.targetNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  checkGuess(guess) {
+    if (guess < this.targetNumber) {
+      return "Too low!";
+    } else if (guess > this.targetNumber) {
+      return "Too high!";
+    } else {
+      return "Correct! You guessed the number.";
+    }
+  }
+}
+
+const game = new GuessingGame();
+
+const guessInput = document.getElementById("guess");
+const submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", () => {
+  const guessValue = parseInt(guessInput.value);
+
+  if (isNaN(guessValue)) {
+    alert("Please enter a valid number.");
+  } else {
+    const result = game.checkGuess(guessValue);
+    alert(result);
+  }
+});
+
+// Getting the min and max range values from the user
+const min = parseInt(prompt("Enter the minimum range value:"));
+const max = parseInt(prompt("Enter the maximum range value:"));
+
+// Setting the range for the game
+game.setRange(min, max);
